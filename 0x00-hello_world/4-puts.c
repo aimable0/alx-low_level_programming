6e75ce2717bb30fc612085ebcf5e6bbedd0c8f21
aimable nkurikiyimana <aimablenkurikiyimana1@gmail.com>
2023-03-19 03:15:30 +0100

    i

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#inlcude < stdio.h >
+ /**
+  * main - A program that prints a line with puts function
+  * Return: 0 (Success)
+  */
+int main(void)
+{
+char message[] = "\"Programming is like building a multilingual puzzle\n";
+puts(message);
+return (0);
+}
