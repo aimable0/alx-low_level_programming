2404b6b2dd7ab6dd651e21993c6199bf90972545
aimable nkurikiyimana <aimablenkurikiyimana1@gmail.com>
2023-03-19 04:27:05 +0100

    done4

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - aprogram that prints out the size of data types
+ *Return: 0 (Sucess)
+ */
+int main(void)
+{
+	int mynum;
+	float myfloat;
+	char mychar;
+
+	printf("lu\n", sizeof(mychar));
+	printf("lu\n", sizeof(myfloat));
+	printf("lu\n", sizeof(mynum));
+	return (0);
+}
