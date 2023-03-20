9909f5da9233b5a1b23553b6df9220624c5fd21f
aimable nkurikiyimana <aimablenkurikiyimana1@gmail.com>
2023-03-20 15:29:39 +0100

    trying

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,11 +6,12 @@
  */
 int main(void)
 {
-char alph[] = "abcdefghijklmnopqrstuvwxyz\n";
+char alph[] = "abcdefghijklmnopqrstuvwxyz";
 int i = 0;
 
 for (i = 0; i < 26; i++)
 putchar(alph[i]);
+putchar('\n');
 
 return (0);
 
