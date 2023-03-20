b5f481349d53def918c87e9f024ae1035e79401e
aimable nkurikiyimana <aimablenkurikiyimana1@gmail.com>
2023-03-20 14:12:47 +0100

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ *main - a program that prints the alphabet in lower case
+ *Return: 0 always (success)
+ */
+int main(void)
+{
+	for (char c = 'a'; c <= 'z'; c++)
+	putchar(c);
+
+	return (0);
+
+}
