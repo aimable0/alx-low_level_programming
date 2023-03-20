a10c6206d6f8d9ea365afc06e19b509728eada5c
aimable nkurikiyimana <aimablenkurikiyimana1@gmail.com>
2023-03-20 23:09:50 +0100

    digitscode

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main - a program that prints digits using asci codes
+ *Return: 0 (success)
+ */
+int main(void)
+{
+int num = 48;
+
+	for (num = 48; num <= 57; num++)
+		putchar(num);
+	putchar('\n');
+
+return (0);
+
+}
