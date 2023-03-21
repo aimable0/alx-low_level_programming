4fcb4fc1ad1f96a7519f6d6b3d8128e718bd44fa
aimable nkurikiyimana <aimablenkurikiyimana1@gmail.com>
2023-03-21 01:10:25 +0100

    done

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,12 +10,14 @@ int i;
 	for (i = 0; i < 10; i++)
 	{
 	putchar(i + '0');
-	if (i < 9)
+	if (i == 9)
 	{
+		continue;
+	}
 	putchar(',');
 	putchar(' ');
 	}
-	}
+	putchar('\n');
 
 return (0);
 
