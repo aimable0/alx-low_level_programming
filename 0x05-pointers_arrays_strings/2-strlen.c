01d99ee5f6eaea6c30975a1fda81f7808f7d24a6
aimable nkurikiyimana <aimablenkurikiyimana1@gmail.com>
2023-03-28 03:18:13 +0200

    creating _strlen function

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *_strlen - returns the length of a string
+ *@*s: any char string
+ *Return: length of a string
+ */
+
+int _strlen(char *s)
+{
+	int _strlen;
+
+	_strlen = strlen(s);
+
+	return (_strlen);
+
+
+}
