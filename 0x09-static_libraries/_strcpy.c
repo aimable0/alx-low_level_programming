ca6ce419757a1a40902f2bbb8634cd0d59bf184a
aimable nkurikiyimana <aimablenkurikiyimana1@gmail.com>
2023-06-12 18:50:57 +0200

    adding the last function that was missing to the library

---
+++ 0x09-static_libraries/_strcpy.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _strcpy - a function that copies a string
+ * @src: the source
+ * @dest: the destination
+ * Return: the copied string
+ */
+char *_strcpy(char *dest, char *src)
+{
+}
