d0223e39c74cd07a9768275110fff0698859aa7c
aimable nkurikiyimana <aimablenkurikiyimana1@gmail.com>
2023-06-12 14:02:45 +0200

    adding more elements in the lib

--- 0x09-static_libraries/_abs.c
+++ 0x09-static_libraries/_abs.c
@@ -1,11 +1,9 @@
 #include "main.h"
 /**
- * _abs : used for certain things
- * @n: any int 
- *
- * Return (int)
+ * _abs - used for certain things
+ * Return: int on success.
+ *@n: intiger number.
  */
 int _abs(int n)
 {
-	
 }
---
+++ 0x09-static_libraries/_isalpha.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ *_isalpha - writes if a character is a an alphabet.
+ *@c: any int
+ *Return: int on success.
+ */
+int _isalpha(int c)
+{
+}
