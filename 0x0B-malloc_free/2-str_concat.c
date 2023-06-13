c7159891c75330a04196568de961128de73dd5cc
aimable nkurikiyimana <aimablenkurikiyimana1@gmail.com>
2023-06-14 01:16:56 +0200

    adding more functions-q

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,7 +14,13 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL || s2 == NULL)
 		return (NULL);
-
+	if (s1 == NULL && s2 != NULL)
+		return (s2);
+	if (s1 != NULL && s2 == NULL)
+		return (s1);
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
+	
 	while (s1[len1] != '\0')
 		len1++;
 
