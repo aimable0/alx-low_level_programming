3f5e1fa82fc90e59fdb1be46e4158f7e89dee286
aimable nkurikiyimana <aimablenkurikiyimana1@gmail.com>
2023-06-13 23:53:56 +0200

    adding more functions of malloc

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *_strdup - a function thatthat returns a pointer to a newly allocated
+ *@str: the string to be copied
+ *Return: pointer to duplicated string, and NULL if it fails
+ */
+char *_strdup(char *str)
+{
+	int i, len = 0;
+	char *st;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	st = malloc(len);
+
+	if (str == NULL)
+		return (NULL);
+
+	else if (str != NULL)
+	{
+		for (i = 0; i < len; i++)
+			st[i] = str[i];
+	}
+
+	return (st);
+}
