0823e68848606d0cc2cf463b4ad859d5a000a89e
aimable nkurikiyimana <aimablenkurikiyimana1@gmail.com>
2023-06-14 00:07:02 +0200

    adding more functions

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat - function that uses malloc to concate stings
+ *@s1: destination
+ *@s2: source
+ *Return: string on success and NULL if it fails.
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i, len1 = 0, len2 = 0, totlen;
+	char *s;
+
+	while (s1[len1] != '\0')
+		len1++;
+
+	while (s2[len2] != '\0')
+		len2++;
+
+	totlen = len1 + len2;
+
+	s = malloc(totlen);
+
+	for (i = 0; i < len1; i++)
+
+	{
+		s[i] = s1[i];
+	}
+
+	for (i = 0; i < len2; i++)
+	{
+		s[len1 + i] = s2[i];
+	}
+	return (s);
+}
