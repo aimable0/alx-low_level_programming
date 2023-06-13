d68325057b723c479fcdf8a913817c51226b75af
aimable nkurikiyimana <aimablenkurikiyimana1@gmail.com>
2023-06-14 00:34:27 +0200

    refining the functions with malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,14 +10,14 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i = 0;
 	char *arr = malloc(size * sizeof(char));
-	
+
 	if (size == 0)
-		return NULL;
+		return (NULL);
 
 	if (arr == NULL)
-		return NULL;
+		return (NULL);
 
-	for (i = 0; i < size; i++) 
+	for (i = 0; i < size; i++)
 		arr[i] = c;
 
 	return (arr);
