af57cef2fba0a2f322dc2382f0bce841a81602d0
aimable nkurikiyimana <aimablenkurikiyimana1@gmail.com>
2023-06-14 14:59:20 +0200

    adding more functions created with argc and argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
-/*
- * main - the main function
- * @argc: the argument count.
- * @rgv: the string argument.
+
+/**
+ * main - the main function of the program.
+ * @argc: the argument count of those passed to main.
+ * @argv: the string argument passed to main function.
  * Return: 0 on succes and 1 on fail
  */
+
 int main(int argc, char *argv[])
 {
-	printf("%d\n", (argc - 1));
+	while (argv)
+	{
+		printf("%d\n", (argc - 1));
+		break;
+	}
 
 	return (0);
 }
