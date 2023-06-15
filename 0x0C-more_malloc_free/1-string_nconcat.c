17a283b69a6687727d2b19f340ed454ae973188e
aimable nkurikiyimana <aimablenkurikiyimana1@gmail.com>
2023-06-15 19:10:26 +0200

    fixing the error in the function

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,7 +10,7 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *str = malloc(sizeof(s1) + n);
+	char *str = malloc(sizeof(s1) + n + 1);
 	unsigned int i;
 	unsigned int len1 = 0, len2 = 0;
 
@@ -22,27 +22,24 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	while (s2[len2] != '\0')
 		len2++;
+	if (n >= len2)
+		n = len2;
 
 	if (s1 == NULL)
 		s1 = "";
 
-	for (i = 0; i < len1; i++)
-		str[i] = s1[i];
-
 	if (s2 == NULL)
 		s2 = "";
 
-	if (n == len2 || n > len2)
-	{
-		for (i = 0; i <= len2; i++)
-		{
-		str[len1 + i] = s2[i];
-		}
-	}
+	for (i = 0; i < len1; i++)
+		str[i] = s1[i];
 
 	for (i = 0; i < n; i++)
 		str[len1 + i] = s2[i];
+
 	str[len1 + n + 1] = '\0';
 
 	return (str);
+
+	free(str);
 }
