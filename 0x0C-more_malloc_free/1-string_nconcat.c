eaff04ba5e34078faedd4d9e0fe86396a21ffb36
aimable nkurikiyimana <aimablenkurikiyimana1@gmail.com>
2023-06-15 20:50:33 +0200

    correcting the function that concatenates strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,19 +13,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *str;
 	unsigned int i, m, len1 = 0, len2 = 0;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
 	while (s1[len1] != '\0')
 		len1++;
 
 	while (s2[len2] != '\0')
 		len2++;
+
 	if (n >= len2)
 		n = len2;
-
-	if (s1 == NULL)
-		s1 = "";
-
-	if (s2 == NULL)
-		s2 = "";
 
 	str = malloc((len1 + n + 1) * sizeof(char));
 	if (str == NULL)
