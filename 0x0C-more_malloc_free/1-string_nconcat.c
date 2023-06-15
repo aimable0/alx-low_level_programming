9016acb6d50450dfb2846c2dfa3882ee7e5b7f14
aimable nkurikiyimana <aimablenkurikiyimana1@gmail.com>
2023-06-15 21:40:43 +0200

    concatenating function with malloc.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,30 +11,30 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    unsigned int len1 = 0, len2 = 0;
-    unsigned int i, j;
-    char *concat;
+	unsigned int len1 = 0, len2 = 0;
+	unsigned int m, j;
+	char *c_st;
 
-    if (s1 != NULL)
-        len1 = strlen(s1);
+	if (s1 != NULL)
+		len1 = strlen(s1);
 
-    if (s2 != NULL)
-        len2 = strlen(s2);
+	if (s2 != NULL)
+		len2 = strlen(s2);
 
-    if (n >= len2)
-        n = len2;
+	if (n >= len2)
+		n = len2;
 
-    concat = malloc((len1 + n + 1) * sizeof(char));
-    if (concat == NULL)
-        return NULL;
+	c_st = malloc((len1 + n + 1) * sizeof(char));
+	if (c_st == NULL)
+		return (NULL);
 
-    for (i = 0; i < len1; i++)
-        concat[i] = s1[i];
+	for (m = 0; m < len1; m++)
+		c_st[m] = s1[m];
 
-    for (j = 0; j < n; j++, i++)
-        concat[i] = s2[j];
+	for (j = 0; j < n; j++, m++)
+		c_st[m] = s2[j];
 
-    concat[i] = '\0';
+	c_st[m] = '\0';
 
-    return concat;
+	return (c_st);
 }
