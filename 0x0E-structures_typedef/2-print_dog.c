0f1904fba032ee7eb91634fa41c17272e0c86804
aimable nkurikiyimana <aimablenkurikiyimana1@gmail.com>
2023-06-21 01:22:51 +0200

    function for printing with struct

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * print_dog - a function for printing with struct
+ *@d: the struct name
+ *Return: void
+ */
+void print_dog(struct dog *d)
+{
+
+	if (d == NULL)
+		exit(98);
+
+	if ((*d).name == NULL)
+		printf("Name: (nil)\n");
+
+	if (d->age == 0.00)
+		printf("nil\n");
+
+	if ((*d).owner == NULL)
+		printf("nil\n");
+
+	while (d != NULL)
+	{
+	printf("Name: %s\n", (*d).name);
+	printf("Age: %f\n", (*d).age);
+	printf("Owner: %s\n", (*d).owner);
+	break;
+	}
+
+}
